b150cd6d8e9e8d1417b64724e2093938bc4592a3
Jamie Hardt <jamiehardt@me.com>
2019-01-01 23:29:20 -0800

    Nudged version, added author

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='wavinfo',
-      version='0.2',
+      version='0.3',
       author='Jamie Hardt',
       author_email='jamiehardt@me.com',
       description='WAVE sound file metadata parser.',
--- wavinfo/__init__.py
+++ wavinfo/__init__.py
@@ -1,1 +1,4 @@
 from .wave_reader import WavInfoReader
+
+__version__ = 0.3
+__author__ = 'Jamie Hardt'
