f12bb0eea4f3a30173134a3901394c402eb1379f
Jamie Hardt <jamiehardt@me.com>
2019-01-03 11:38:08 -0800

    Update setup.py
    Next will be version 1.0

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='wavinfo',
-      version='0.3',
+      version='1.0',
       author='Jamie Hardt',
       author_email='jamiehardt@me.com',
       description='WAVE sound file metadata parser.',
       long_description_content_type="text/markdown",
       long_description=long_description,
       url='https://github.com/iluvcapra/wavinfo',
-      classifiers=['Development Status :: 4 - Beta',
+      classifiers=['Development Status :: 5 - Production/Stable',
           'License :: OSI Approved :: MIT License',
           'Topic :: Multimedia',
 	  'Topic :: Multimedia :: Sound/Audio',
