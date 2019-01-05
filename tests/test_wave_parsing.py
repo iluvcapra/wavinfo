4f0c26f5caa695d0336f394c8a66332329a97517
Jamie Hardt <jamiehardt@me.com>
2019-01-05 10:27:29 -0800

    Update test_wave_parsing.py
    2.7 test support

--- tests/test_wave_parsing.py
+++ tests/test_wave_parsing.py
@@ -1,4 +1,5 @@
 import os.path
+import sys
 import json
 import subprocess
 from subprocess import PIPE
@@ -13,15 +14,25 @@ FFPROBE='ffprobe'
 def ffprobe(path):
 
     arguments = [ FFPROBE , "-of", "json" , "-show_format", "-show_streams", path ]
+    
+    if int(sys.version[0]) <  3:
+        process = subprocess.Popen(arguments, stdout=PIPE)
+        process.wait()
+        if process.returncode == 0:
+            output = process.communicate()[0] 
+            if output:
+                output_str = output.decode('utf-8')
+                return json.loads(output_str)
+        else:
+            return None
+    else: 
+        process = subprocess.run(arguments, stdin=None, stdout=PIPE, stderr=PIPE)
 
-    process = subprocess.run(arguments, stdin=None, stdout=PIPE, stderr=PIPE)
-
-    if process.returncode == 0:
-        output_str = process.stdout.decode('utf-8')
-        return json.loads(output_str)
-    else:
-        return None
-
+        if process.returncode == 0:
+            output_str = process.stdout.decode('utf-8')
+            return json.loads(output_str)
+        else:
+            return None
 
 class TestWaveInfo(TestCase):
 
