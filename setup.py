6a69df2ee8069378d1ef000c6dc202fa7723a0ff
Ronald van Elburg <Ronald@SoundAppraisal.eu>
2022-10-17 13:28:28 +0200

    Move version and author information to separate file. The current location leads to problems with dependencies which are only resolved after running pip.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(name='wavinfo',
           "Programming Language :: Python :: 3.9",
           "Programming Language :: Python :: 3.10"],
       keywords='waveform metadata audio ebu smpte avi library film tv editing editorial',
-      install_requires=['lxml', 'ear'],
+      install_requires=['lxml', 'wheel', 'ear'],
       entry_points={
           'console_scripts': [
               'wavinfo = wavinfo.__main__:main'
