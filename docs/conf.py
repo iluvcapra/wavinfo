df90c67a73bd6412b890e7e952ee644b5f4410fa
Jamie Hardt <jamiehardt@me.com>
2022-11-16 19:33:01 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'), os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
 print(sys.path)
 
 
@@ -62,7 +63,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
