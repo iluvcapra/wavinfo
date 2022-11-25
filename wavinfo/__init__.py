8fe7eefb4a043f018db116922a0a961e882f7b0c
Jamie Hardt <jamiehardt@me.com>
2022-11-24 22:06:44 -0800

    Docs

--- wavinfo/__init__.py
+++ wavinfo/__init__.py
@@ -1,7 +1,7 @@
 """
 methods to probe a WAV file for various kinds of production metadata.
 
-Go to the documentation for wavinfo.WavInfoReader for more information.
+See the documentation for `wavinfo.WavInfoReader` for more information.
 """
 
 from .wave_reader import WavInfoReader
