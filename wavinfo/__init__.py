3c46d8130200e600599b71ce7f87e89ae409af83
Jamie Hardt <jamiehardt@me.com>
2023-02-26 11:35:23 -0800

    Bumped version to 2.2.0

--- wavinfo/__init__.py
+++ wavinfo/__init__.py
@@ -5,5 +5,5 @@ Probe WAVE Files for iXML, Broadcast-WAVE and other metadata.
 from .wave_reader import WavInfoReader
 from .riff_parser import WavInfoEOFError
 
-__version__ = '2.1.1'
-__short_version__ = '2.1'
+__version__ = '2.2.0'
+__short_version__ = '2.2.0'
