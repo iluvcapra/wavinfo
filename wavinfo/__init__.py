a196e4786ed3d120ab69af16018be392ff9147a0
Jamie Hardt <jamiehardt@me.com>
2023-11-08 21:16:36 -0800

    Update __init__.py
    Nudged version

--- wavinfo/__init__.py
+++ wavinfo/__init__.py
@@ -5,5 +5,5 @@ Probe WAVE Files for iXML, Broadcast-WAVE and other metadata.
 from .wave_reader import WavInfoReader
 from .riff_parser import WavInfoEOFError
 
-__version__ = '2.3.0'
+__version__ = '2.3.1'
 __short_version__ = '2.3.0'
