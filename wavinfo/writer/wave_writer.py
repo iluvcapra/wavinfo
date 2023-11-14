86aab4bdfbc8c78a00904dec52d8f43e5be8a737
Jamie Hardt <jamiehardt@me.com>
2023-11-13 18:04:40 -0800

    Chunk writing implementation IP

--- wavinfo/writer/wave_writer.py
+++ wavinfo/writer/wave_writer.py
@@ -9,7 +9,9 @@ from ..reader.riff_parser import parse_chunk, ChunkDescriptor, ListChunkDescript
 from enum import Enum
 from struct import pack
 from io import BufferedRandom, SEEK_CUR, SEEK_SET
-from typing import Tuple
+from typing import Tuple, Optional
+
+JUNK_IDENTS = (b'JUNK', b'FLLR', b'PAD ')
 
 
 class WavInfoWriter:
@@ -17,20 +19,21 @@ class WavInfoWriter:
     An in-place WAVE file chunk writer/updater.
     """
 
-    #: Size of initial ``JUNK`` chunk to avoid overwriting. 
+    #: Size of initial ``JUNK`` chunk to avoid overwriting.
     ds64_reservation_size: int = 64
     preserve_ds64_reservation: bool = True
-    file: BufferedRandom 
+    file: BufferedRandom
 
     class Placement(Enum):
         #: Place new chunk as early in the file as possible.
-        HEAD = 1
-        
-        #: Place new chunk at the end.
-        TAIL = 2
-   
+        FIRST_AVAILABLE = 1
+
+        #: Place new chunk after `data`
+        AFTER_DATA = 2
+
+
     def __init__(self, f, preserve_ds64_reservation: bool = True):
-        if isinstance(f, BufferedRandom):
+        if hasattr(f, 'read') and hasattr(f, 'write'):
             self.file = f
         else:
             self.file = open(f, "r+b")
@@ -39,60 +42,106 @@ class WavInfoWriter:
         main_list = parse_chunk(self.file)
         assert isinstance(main_list, ListChunkDescriptor)
         assert not main_list.is_rf64(), ("RF64 metadata writing is not "
-                                          "supported")
+                                         "supported")
 
     def erase_chunk(self, ident: bytes, index: int):
         """
-        Strike-out a chunk with a ``JUNK`` chunk, and then combine with 
+        Strike-out a chunk with a ``JUNK`` chunk, and then combine with
         adjacent ``JUNK`` if there.
         """
+        self.file.seek(0, SEEK_SET)
+        main_list = parse_chunk(self.file)
+        assert isinstance(main_list, ListChunkDescriptor)
+
+        i = 0
+        for chunk in main_list.children:
+            if not isinstance(chunk, ChunkDescriptor):
+                continue
+            if chunk.ident == ident:
+                if i == index:
+                    self.file.seek(chunk.start - 8, SEEK_SET)
+                    self.file.write(b"JUNK")
+                else:
+                    i += 1
+
+        self.file.flush()
+        self._combine_adjacent_junk()
+
+    def erase_list_form(self, ident: bytes, index: int):
+        """
+        Strike-out a ``LIST`` chunk form with a signature ``ident``.
+        """
         pass
 
-    def rubout_list_form(self, ident: bytes, index: int):
+    def write_chunk(self, ident: bytes, data: bytes,
+                    placement: Placement = Placement.FIRST_AVAILABLE):
         """
-        Strike-out a ``LIST`` chunk form with a signature ``ident``. 
+        Writes a new chunk
         """
-        pass
+        if placement == self.Placement.FIRST_AVAILABLE:
+            target = self._first_available_junk(sized_for=len(data))
+            if target is not None:
+                if target[1] is True:
+                    self._overwrite_junk(target[0], ident, data)
+                else:
+                    self._split_junk(target[0], len(data))
+                    self._overwrite_junk(target[0], ident, data)
 
-    def write_chunk(self, ident: bytes, data: bytes, 
-                    placement: Placement = Placement.HEAD):
-        """
-        Writes a new chunk 
-        """
-        if placement == self.Placement.HEAD:
+        elif placement == self.Placement.AFTER_DATA:
             pass
-        elif placement == self.Placement.TAIL:
+        elif placement == self.Placement.AT_END:
             pass
+
+    def _first_available_junk(self,
+                              sized_for: int) -> Optional[Tuple[int, bool]]:
+        """
+        Find the first available ``JUNK`` large enough to for free space to be
+        allocated to a chunk of size ``sized_for``. A ``JUNK`` chunk is
+        available if it can be split into a chunk ``sized_for``
+
+        :returns: index of found chunk, and bool indicating if the size match
+            is exact.
+        """
+        junks = self._junk_list()
+        for i, junk in enumerate(junks):
+            if self._can_split_junk(i, sized_for):
+                return i, False
+            elif junk.length in [sized_for, sized_for + 1]:
+                return i, True
+
+        return None
 
     def _split_junk(self, index: int, at: int):
         """
         Split a ``JUNK`` chunk. After splitting, file will have two ``JUNK``
-        chunks, one at ``index`` and one at ``index + 1``. 
+        chunks, one at ``index`` and one at ``index + 1``.
 
         ``JUNK`` at ``index`` will have size ``at + (at % 2)``, all remaining
         space (mins 8 bytes of chunk overhead) will be allocated to ``JUNK``
-        at ``index + 1``. This space must be greater than or equal to the 
-        minimum chunk size, two bytes, and this is asserted. To check this 
+        at ``index + 1``. This space must be greater than or equal to the
+        minimum chunk size, two bytes, and this is asserted. To check this
         without an assertion use ``_can_split_junk()``.
         """
-        assert self._can_split_junk(index, at), (f"Cannot split JUNK chunk at "
-            "index {index}, insufficient remaining size")
+        assert self._can_split_junk(
+            index, at), (f"Cannot split JUNK chunk at "
+                         "index {index}, insufficient remaining size")
 
         target = self._get_junk_at(index)
 
         self.file.seek(target[0] - 4, SEEK_SET)
         head_size = at + at % 2
-        tail_size = at - (head_size + 8)
+        tail_size = target[1] - (at + 8)
         self.file.write(pack("<I", head_size))
         self.file.seek(at, SEEK_CUR)
         self.file.write(b"JUNK")
         self.file.write(pack("<I", tail_size))
         self.file.flush()
-        
+
     def _can_split_junk(self, index: int, at: int) -> bool:
         """
-        Tests if the ``JUNK`` chunk at ``index`` can be split at position 
-        ``at``, and leave enough space for the second.
+        Tests if the ``JUNK`` chunk at ``index`` can be split at position
+        ``at``, and leave enough space for the second. ``at`` will be rounded
+        up to the nearest even number.
         """
         target = self._get_junk_at(index)
 
@@ -103,36 +152,36 @@ class WavInfoWriter:
         target_len = target[1]
         size_head = at + at % 2
         size_tail = target_len - (size_head + HEADER_SIZE)
-        
+
         return size_tail >= MIN_JUNK_SIZE
 
     def _get_junk_at(self, index: int) -> Tuple[int, int]:
         """
         Get the location of a ``JUNK`` chunk.
-        
+
         :param index: The index of the ``JUNK`` chunk to find.
-        :returns: A Tuple with the offset of the requested ``JUNK`` chunk's 
+        :returns: A Tuple with the offset of the requested ``JUNK`` chunk's
             payload, and the length of the payload.
         """
         junks = self._junk_list()
 
         assert len(junks) > index, f"No JUNK chunk at index {index}"
-        
+
         return (junks[index].start, junks[index].length)
 
     def _overwrite_junk(self, index: int, new_ident: bytes, data: bytes):
         """
-        Overwrite a ``JUNK`` chunk. The chunk overwritten must be exactly 
+        Overwrite a ``JUNK`` chunk. The chunk overwritten must be exactly
         ``len(data)`` size, or one more than ``len(data)``.
         """
         target = self._get_junk_at(index)
         assert len(data) in (target[1], target[1] - 1), \
-        (f"JUNK chunk at index {index} is not correct size for data len "
-         "{len(data)}")
+            (f"JUNK chunk at index {index} is not correct size for data len "
+             "{len(data)}")
 
         self.file.seek(target[0] - 8, SEEK_SET)
         self.file.write(new_ident)
-        self.file.write(pack("<H", len(data)))
+        self.file.write(pack("<I", len(data)))
         self.file.write(data)
         self.file.flush()
 
@@ -143,20 +192,26 @@ class WavInfoWriter:
         self.file.seek(0, SEEK_SET)
         main_list = parse_chunk(self.file)
         assert isinstance(main_list, ListChunkDescriptor)
-        assert False, "Implementation in progress" # FIXME
-
-    def _rubout_chunk(self, index: int):
-        target = self._get_junk_at(index)
-
-        self.file.seek(target[0] - 8, SEEK_SET)
-        self.file.write(b"JUNK")
-        self.file.flush()
+        assert False, "Implementation in progress"  # FIXME
 
     def _combine_adjacent_junk(self):
         """
         Combine adjacent ``JUNK`` chunks into single chunks.
         """
-        pass
+        l = self._junk_list()
+        if len(l) == 1:
+            return
+
+        i = 0
+        while i + 1 < len(self._junk_list()):
+            (a, b) = (self._get_junk_at(i), self._get_junk_at(i + 1))
+            if a[0] + a[1] + 8 == b[0]:
+                self.file.seek(a[0] - 4, SEEK_SET)
+                new_a_size = a[1] + 8 + b[1]
+                self.file.write(pack("<I", new_a_size))
+                self.file.flush()
+            else:
+                i += 1
 
     def _optimize(self):
         """
@@ -169,5 +224,6 @@ class WavInfoWriter:
     def _junk_list(self):
         self.file.seek(0, SEEK_SET)
         main_list = parse_chunk(self.file)
-        return [c for c in main_list 
-                if isinstance(c, ChunkDescriptor) and c.ident ==b"JUNK"]
+        assert isinstance(main_list, ListChunkDescriptor)
+        return [c for c in main_list.children
+                if isinstance(c, ChunkDescriptor) and c.ident in JUNK_IDENTS]
