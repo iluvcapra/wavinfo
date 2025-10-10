7393afac959673b4eff257cd90d6a52bed5e0274
Jamie Hardt <jamie@squad51.us>
2025-10-09 23:17:46 -0700

    Docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import importlib
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath("../../.."))
+sys.path.insert(0, os.path.abspath("../../../src"))
 print(sys.path)
 
 import importlib
